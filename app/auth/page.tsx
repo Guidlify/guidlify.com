9b3611bf08203377ee95a44aec6aaa0aeb09f216
Anmol Baranwal <anmolbaranwal119@gmail.com>
2023-10-22 13:23:22 +0530

    chore: correct cta routes with minor changes

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -18,6 +18,10 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   const { formType, setFormType } = useContext(AuthPageContext)
 
+  const handleSignup = () => {
+    setFormType("signup")
+  }
+
   return (
     <>
       <div className="container relative flex h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
@@ -36,37 +40,36 @@ export default function AuthenticationPage() {
               <h1 className="text-2xl font-semibold tracking-tight">
                 {formType === "signup" ? "Hey! What's up? 👋" : "Hello! 👋"}
               </h1>
-              <p className="text-sm text-muted-foreground">
-                {formType === "signup"
-                  ? "It's time for you create an account. Let's get started!"
-                  : "Welcome back to WebX Guild! We are excited to see you again."}
-              </p>
+              {formType === "signup" ? (
+                <div className="text-sm text-muted-foreground">
+                  <p>Its time for you to create an account.</p>
+                  <p>Lets get started!</p>
+                </div>
+              ) : (
+                <div className="text-sm text-muted-foreground">
+                  <p>Welcome back to WebX Guild!</p>
+                  <p>We are excited to see you again.</p>
+                </div>
+              )}
             </div>
             <UserAuthForm formType={formType} onSuccessfulAuth={() => {}} />
-            <Link
-              href="#"
-              onClick={(e) => {
-                e.preventDefault()
-                setFormType(formType === "login" ? "signup" : "login")
-              }}
-            >
-              <div className="mr-2 mt-10 flex items-end justify-end space-x-2">
-                {formType === "login" && (
-                  <Button
-                    //   disabled={isLoading}
-                    className="w-24 border-slate-400 hover:border hover:bg-transparent hover:text-white"
-                  >
-                    Sign In
-                  </Button>
-                )}
+            <div className="mr-2 mt-16 flex items-end justify-end space-x-2 pt-10">
+              {formType === "login" && (
                 <Button
                   //   disabled={isLoading}
-                  className="w-24 border border-slate-400 bg-transparent text-white hover:text-black"
+                  className="w-24 border-slate-400 hover:border hover:bg-transparent hover:text-white"
                 >
-                  Sign Up
+                  Sign In
                 </Button>
-              </div>
-            </Link>
+              )}
+              <Button
+                //   disabled={isLoading}
+                className="w-24 border border-slate-400 bg-transparent text-white hover:text-black"
+                onClick={handleSignup}
+              >
+                Sign Up
+              </Button>
+            </div>
             {formType === "signup" && (
               <p className="px-2 pt-8 text-center text-sm text-muted-foreground">
                 <Link
