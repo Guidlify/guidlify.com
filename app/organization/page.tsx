3d2d0d545a933418038e19d2a6dabc3faf8877ba
Anmol Baranwal <anmolbaranwal119@gmail.com>
2023-11-26 12:14:55 +0530

    chore: add new logo assets

---
+++ app/organization/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const OrganizationPage = () => {
+  return (
+    <div className="container">
+      <section className="grow space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-28">
+        hey
+      </section>
+    </div>
+  )
+}
+
+export default OrganizationPage
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,80 +1,75 @@
 import * as React from "react"
-
-import { Icons } from "@/components/icons"
-import { ModeToggle } from "@/components/mode-toggle"
-import { cn } from "@/lib/utils"
 import Image from "next/image"
 
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+import { ModeToggle } from "@/components/mode-toggle"
+
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
-	const footerData = [
-		{
-			title: "About",
-			items: ["Company", "Store", "Open Source", "Research"],
-		},
-		{
-			title: "Resources",
-			items: ["Tools", "Blog", "Tutorials", "FAQs"],
-		},
-		{
-			title: "Support",
-			items: ["Contact Us", "Developers", "Documentation", "Integrations"],
-		},
-		{
-			title: "Legal",
-			items: ["Policy", "Terms", "Privacy", "Cookies"],
-		},
-	]
-	return (
-		<footer className={cn(className)}>
-			<div className="container flex flex-col items-center justify-between gap-4 md:flex-row md:py-0">
+  const footerData = [
+    {
+      title: "About",
+      items: ["Company", "Store", "Open Source", "Research"],
+    },
+    {
+      title: "Resources",
+      items: ["Tools", "Blog", "Tutorials", "FAQs"],
+    },
+    {
+      title: "Support",
+      items: ["Contact Us", "Developers", "Documentation", "Integrations"],
+    },
+    {
+      title: "Legal",
+      items: ["Policy", "Terms", "Privacy", "Cookies"],
+    },
+  ]
+  return (
+    <footer className={cn(className)}>
+      <div className="container flex flex-col items-center justify-between gap-4 md:flex-row md:py-0">
+        <div className="flex flex-1 flex-col items-center gap-4 md:flex-row md:gap-2 md:px-0 lg:items-start">
+          <div className="mr-4 xs:col-span-5 xs:pb-8 lg:col-span-1 lg:pb-0">
+            <Image
+              src={"/guidlify-brand-logo.svg"}
+              alt=""
+              width={150}
+              height={80}
+            />
+          </div>
 
+          <div className="mx-auto flex w-full flex-wrap items-center justify-between gap-4 px-4 pb-4">
+            {footerData.map((column, index) => (
+              <div
+                key={index}
+                className="flex flex-col gap-2 xs:col-span-5 xs:w-full xs:text-center sm:col-span-2 sm:w-auto sm:shrink-0 sm:pb-0 sm:text-left lg:col-span-1"
+              >
+                <p className="font-semibold">{column.title}</p>
+                {column.items.map((item, itemIndex) => (
+                  <p
+                    key={itemIndex}
+                    className="md:text-md cursor-pointer text-sm transition duration-200 hover:underline"
+                  >
+                    {item}
+                  </p>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <ModeToggle />
+      </div>
 
-				<div className="flex flex-col items-center lg:items-start gap-4 md:flex-row md:gap-2 md:px-0 flex-1">
-
-					<div className="xs:col-span-5 xs:pb-8 lg:col-span-1 lg:pb-0">
-						<Image
-							src={"/guildify-brand-logo.svg"}
-							alt=""
-							width={120}
-							height={50}
-						/>
-					</div>
-
-					<div className="w-full mx-auto flex flex-wrap gap-4 justify-between items-center px-4 pb-4">
-						{footerData.map((column, index) => (
-							<div
-								key={index}
-								className="flex flex-col xs:w-full sm:w-auto sm:flex-shrink-0 xs:col-span-5 xs:text-center sm:col-span-2 sm:pb-0 sm:text-left lg:col-span-1 gap-2"
-							>
-								<p className="font-semibold">{column.title}</p>
-								{column.items.map((item, itemIndex) => (
-									<p
-										key={itemIndex}
-										className="text-sm md:text-md cursor-pointer transition duration-200 hover:underline"
-									>
-										{item}
-									</p>
-								))}
-							</div>
-						))}
-					</div>
-				</div>
-				<ModeToggle />
-			</div>
-
-			<div className="container flex flex-col items-center justify-between gap-4 md:flex-row  md:border-t md:border-muted-foreground/30 py-4">
-				<h1 className="flex">
-					<div className="mr-2">
-						<Icons.Discord className="h-6 w-6 text-[#5865F2]" />
-					</div>
-					<div className="mr-2">
-						<Icons.X className="h-5 w-5" />
-					</div>
-				</h1>
-				<h1 className="xs:text-md sm:text-sm">
-					Copyright © 2023 Guidlify
-				</h1>
-			</div>
-		</footer>
-	)
+      <div className="container flex flex-col items-center justify-between gap-4 py-4  md:flex-row md:border-t md:border-muted-foreground/30">
+        <h1 className="flex">
+          <div className="mr-2">
+            <Icons.Discord className="h-6 w-6 text-[#5865F2]" />
+          </div>
+          <div className="mr-2">
+            <Icons.X className="h-5 w-5" />
+          </div>
+        </h1>
+        <h1 className="xs:text-md sm:text-sm">Copyright © 2023 Guidlify</h1>
+      </div>
+    </footer>
+  )
 }
